547e0f4356f39ba6f82d5d19b670e74fd3930c6b
Shaz'e <shazinabbas_m@srmap.edu.in>
2023-02-05 15:25:45 +0530

    Pilot commit

---
+++ src/test.java
@@ -0,0 +1,11 @@
+public class test {
+    //print hey using stars and spaces
+    public static void printHey()
+    {
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
